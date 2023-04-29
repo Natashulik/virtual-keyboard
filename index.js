77e92a50735130916da7bc1d22a725551a47b9fb
Natasha Victorovich <nata17v@mail.ru>
2023-04-29 13:17:46 +0300

    feat: implement sound, key_delete

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const body = document.querySelector('body');
 const screen = document.createElement('div');
 const keyboard = document.createElement('div');
 
+
 screen.className = 'screen';
+screen.contentEditable = true;
+
+
 keyboard.className = 'keyboard';
 
 body.appendChild(screen);
 body.appendChild(keyboard);
+
+
 
 /* add lines */
 
@@ -32,16 +38,22 @@ line5.className = 'line line5';
 keyboard.appendChild(line5);
 
 /* add keys */
-const arrKeys1 = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
-arrKeys1.forEach((item) => {
+const arrKeys = [
+  ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+  ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'DEL'],
+  ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'ENTER'],
+  ['Shift', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '.', ',', '/', '⇧', 'Shift'],
+  ['Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '⇦', '⇩', '⇨']
+]
+
+arrKeys[0].forEach((item) => {
   const key = document.createElement('div');
   key.className = 'key';
   key.innerText = item;
   line1.appendChild(key);
 });
 
-const arrKeys2 = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'DEL'];
-arrKeys2.forEach((item, index) => {
+arrKeys[1].forEach((item, index) => {
   const key = document.createElement('div');
   key.className = 'key';
   if (index >= 1 && index <= 10) {
@@ -51,8 +63,7 @@ arrKeys2.forEach((item, index) => {
   line2.appendChild(key);
 });
 
-const arrKeys3 = ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'ENTER'];
-arrKeys3.forEach((item, index) => {
+arrKeys[2].forEach((item, index) => {
   const key = document.createElement('div');
   key.className = 'key';
   if (index >= 1 && index <= 9) {
@@ -62,8 +73,7 @@ arrKeys3.forEach((item, index) => {
   line3.appendChild(key);
 });
 
-const arrKeys4 = ['Shift', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '.', ',', '/', '⇧', 'Shift'];
-arrKeys4.forEach((item, index) => {
+arrKeys[3].forEach((item, index) => {
   const key = document.createElement('div');
   key.className = 'key';
   if (index >= 2 && index <= 8) {
@@ -73,10 +83,10 @@ arrKeys4.forEach((item, index) => {
   line4.appendChild(key);
 });
 
-const arrKeys5 = ['Ctrlt', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '⇦', '⇩', '⇨'];
-arrKeys5.forEach((item, index) => {
+arrKeys[4].forEach((item, index) => {
   const key = document.createElement('div');
   key.className = 'key';
+  if (index === 2) { key.className = 'key alt_left'; }
   if (index === 3) { key.className = 'key space'; }
   key.innerText = item;
   line5.appendChild(key);
@@ -87,6 +97,12 @@ arrKeys5.forEach((item, index) => {
 const keys = document.querySelectorAll('.key');
 const letters = document.querySelectorAll('.letter');
 
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/648");
+let altPressed = false;  //звук изначально отключен
+let sound = function() {
+  audio.play();
+};
+
 keys.forEach((key) => {
   key.addEventListener('click', function pressKey() {
     if (this.innerText === 'Caps Lock') { // включение и выключение capslock
@@ -96,11 +112,36 @@ keys.forEach((key) => {
       });
     } else if (this.innerText === 'Backspace') { // работа Backspace
       const str = screen.innerText;
-      screen.innerText = str.substring(0, str.length - 1);
-    } else if (this.className === 'key space') { // работа пробела
+      screen.innerText = str.slice(0, str.length - 1);
+    }
+    else if (this.innerText === 'DEL') { // работа DELETE
+      const str = screen.innerText;
+      screen.innerText = str.slice(0, -1);
+    }
+    else if (this.className === 'key space') { // работа пробела
       screen.innerText += ' ';
-    } else {
+    }
+    else if (this.className === 'key alt_left') {
+      if(!altPressed) {
+        keys.forEach(key => {
+          key.addEventListener("click", sound);
+        });
+        altPressed = true;
+      } else if(altPressed){
+        keys.forEach(key => {
+          key.removeEventListener("click", sound);
+        });
+        altPressed = false;
+      }
+    }
+    
+    else {
       screen.innerText += this.innerText;
     }
   });
 });
+
+
+
+
+
